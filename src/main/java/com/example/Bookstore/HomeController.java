6a97407ca8fb131f39b36ffd8610df03342d3ae3
Rcorporan89 <Rcorporan89@gmail.com>
2023-04-11 16:18:12 -0400

    adding a search form to home pg for searching books from books pg.

--- src/main/java/com/example/Bookstore/HomeController.java
+++ src/main/java/com/example/Bookstore/HomeController.java
@@ -1,10 +1,13 @@
 package com.example.Bookstore;
 
+import com.example.Bookstore.model.Inquiry;
 import com.example.Bookstore.service.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -13,13 +16,21 @@ public class HomeController {
     private HomeService homeService;
 
     @GetMapping("/index")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("inquiry",new Inquiry());
         return "index";
     }
 
     @GetMapping("/books")
     public String books(Model model){
         model.addAttribute("books", homeService.getBooks());
+        return "books";
+    }
+
+    @PostMapping("/books")
+    public String searchBooks(Model model, @ModelAttribute Inquiry inquiry){
+        String bookTitle = inquiry.getSearchString();
+        model.addAttribute("books", homeService.searchedBooks(bookTitle));
         return "books";
     }
 
